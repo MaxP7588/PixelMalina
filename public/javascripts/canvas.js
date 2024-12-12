e382d723eb8365b601f7344bb4f32ab2c115b6d1
Patricio Maximiliano Zambrano Rubilar <patricio.zambrano2001@alumnos.ubiobio.cl>
2024-12-12 12:44:35 -0300

    init

---
+++ public/javascripts/canvas.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+  var canvas = document.getElementById('pixelCanvas');
+  var context = canvas.getContext('2d');
+  // Aquí puedes agregar el código para manejar el dibujo en el canvas
+};
---
+++ routes/canvas.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  var template = req.query.template;
+  var width = req.query.width;
+  var height = req.query.height;
+  res.render('canvas', { template: template, width: width, height: height });
+});
+
+module.exports = router;
